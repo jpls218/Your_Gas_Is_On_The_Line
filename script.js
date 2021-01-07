d154b4a6ea792d4eefce9040b69ca4deb8441122
Curt Spiegelhalter <i16dcurt@gmail.com>
2021-01-07 13:33:30 -0600

    Added geolocation function

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-$(document).ready(function(){
+window.onload = function(){
+
+    getLocation()
+
+    var coords = []
+
+    function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+          
+        } else { 
+          x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+        
+      }
+    function showPosition(position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+          coords = [lat, long]
+          showMap(coords);
+    }
 
 
+    function showMap(coords) {
 
-    L.mapquest.key = '0GxkVNACX7ZbVYmlAotnqVBEvhPvDi1j';
+        L.mapquest.key = '0GxkVNACX7ZbVYmlAotnqVBEvhPvDi1j';
 
-    // 'map' refers to a <div> element with the ID map
-    L.mapquest.map('map', {
-    center: [37.7749, -122.4194],
-    layers: L.mapquest.tileLayer('map'),
-    zoom: 12
-    });
+        // 'map' refers to a <div> element with the ID map
+        let map = L.mapquest.map('map', {
+        center: [coords[0], coords[1]],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12
+        });
 
-})
+}
